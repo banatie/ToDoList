788ae30bffaf6f421ca45d8239e247b11495fafc
eiji.iriyama <eiji.iriyama@zmp.co.jp>
2020-11-08 00:35:04 +0900

    Sends different html when logged in

--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, 'todolist/index.html', context)
-    
+
 def signup(request):
     context = {}
     return render(request, 'todolist/signup.html', context)
+
