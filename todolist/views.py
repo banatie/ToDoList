84828ec9a564a7a4ad4d53c1b353d511be7a3c3f
eiji.iriyama <eiji.iriyama@zmp.co.jp>
2020-11-07 22:17:19 +0900

    Show index.html using templates and static

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'todolist/index.html', context)
+    
+def signup(request):
+    return HttpResponse('signup!!!')
