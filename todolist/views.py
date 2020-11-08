4bd8351329c8b660642ef6187a6837dec4ce5a0a
iriyama <eiji.iriyama.japan@gmail.com>
2020-11-08 17:47:27 +0900

    Add Signup Feature

--- todolist/views.py
+++ todolist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,33 @@ def index(request):
     return render(request, 'todolist/index.html', context)
 
 def signup(request):
-    context = {}
-    return render(request, 'todolist/signup.html', context)
+    if request.user.is_authenticated:
+        context = {}
+        return render(request, 'todolist/index.html', context)
+    
+    if request.method == 'GET':
+        context = {}
+        return render(request, 'todolist/signup.html', context)
+    elif request.method == 'POST':
+        try:
+            email = request.POST['email']
+            password = request.POST['password']
+        except KeyError:
+            context = {'error_message' : 'Email or Password is not Found'}
+            return render(request, 'todolist/signup.html', context)
+        user = User.objects.create_user(username=email, password=password)
+        auth_login(request, user)
+        context = {}
+        return render(request, 'todolist/index.html', context)
+        """
+        if user is not None:
+            login(request, user)
+            context = {}
+            return render(request, 'todolist/index.html', context)
+        else:
+            context = {'error_message' : 'Authentication Failed'}
+            return render(request, 'todolist/signup.html', context)
+        """
 
 def login(request):
     # Only Accept Post Request
@@ -23,6 +49,7 @@ def login(request):
     password = request.POST.get('password')
     user = authenticate(request, username=email, password=password)
     if user is not None:
+        login(request, user)
         context = {}
         return render(request, 'todolist/index.html', context)
     else:
