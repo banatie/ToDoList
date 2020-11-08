394a9ce287bafd7daa1ada7994ffeb194c876411
iriyama <eiji.iriyama.japan@gmail.com>
2020-11-08 17:07:54 +0900

    Removed cached file

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+#from django.http import HttpResponse
+#from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,19 @@ def signup(request):
     context = {}
     return render(request, 'todolist/signup.html', context)
 
+def login(request):
+    # Only Accept Post Request
+    if request.method != 'POST':
+        context = {'error_message' : 'Invalid HTTP request'}
+        render(request, 'todolist/index.html', context)
+
+    # Get email, password
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+        context = {}
+        return render(request, 'todolist/index.html', context)
+    else:
+        context = {'error_message' : 'Account not Found.'}
+        return render(request, 'todolist/index.html', context)
