5ed7d6ac8560ff10672f7508703c19670cdcc24b
iriyama <eiji.iriyama.japan@gmail.com>
2020-12-09 01:31:51 +0900

    Add importing local_settings.py if not product env

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
